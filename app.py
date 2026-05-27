6c62683ac85d4e56df1146aaeacf965d5a050109
Miguel Valadão <108155433+MiguelValadao@users.noreply.github.com>
2026-05-27 10:24:06 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request # type: ignore
+from calculadora import calcular
+
+app = Flask(__name__)
+
+@app.route('/', methods=["GET", "POST"]) 
+def index():
+    if request.method == "POST":
+        return calcular()
+    return render_template('calculator.html', etapas="", resultado="")
+
+if __name__ == '__main__':
+    app.run(debug=True)
